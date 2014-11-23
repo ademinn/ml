4e99dd31f06cff4b575625f3df50b991a2821497
Andrey Demin <ademinn@gmail.com>
2014-11-23 22:17:31 +0300

    removed boxes

--- src/knn/main.rs
+++ src/knn/main.rs
@@ -10,18 +10,18 @@ fn distance(p1: &Point, p2: &Point) -> f64 {
     ((p1.x - p2.x).powi(2i32) + (p1.y - p2.y).powi(2i32)).sqrt()
 }
 
-fn solve(points: &vec::Vec<Box<(Point, i32)>>, point: &Point, dist: |&Point, &Point| -> f64) -> int {
+fn solve(points: &vec::Vec<(Point, i32)>, point: &Point, dist: |&Point, &Point| -> f64) -> int {
     let mut sorted = vec::Vec::new();
-    for v in points.iter() {
+    for &v in points.iter() {
         sorted.push(v);
     }
-    sorted.sort_by(|&& box (p1, _), && box (p2, _)| -> Ordering {
+    sorted.sort_by(|&(p1, _), &(p2, _)| -> Ordering {
         match dist(&p1, point).partial_cmp(&dist(&p2, point)) {
             Some(x) => x,
             None => panic!("NaN value"),
         }
     });
-    for && box (p, _) in sorted.iter() {
+    for &(p, _) in sorted.iter() {
         println!("{}", p.x);
     }
     return 1
@@ -31,7 +31,7 @@ fn solve(points: &vec::Vec<Box<(Point, i32)>>, point: &Point, dist: |&Point, &Po
 fn main() {
     let mut points = vec::Vec::new();
     for i in range(0i, 10i) {
-        let p = box () (Point {x: i as f64, y: 0.0f64}, 0i32);
+        let p = (Point {x: i as f64, y: 0.0f64}, 0i32);
         points.push(p);
     }
     solve(&points, &Point{x: 10.0f64, y: 0.0f64}, distance);
