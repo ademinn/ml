3ae946f8b7f874e24f0672b06655171e8459cd91
Andrey Demin <ademinn@gmail.com>
2014-11-29 23:26:31 +0300

    knn with leave-out-out

--- src/knn/main.rs
+++ src/knn/main.rs
@@ -1,14 +1,19 @@
+use std::os;
+use std::io::BufferedReader;
+use std::io::File;
 use std::num::Float;
 use std::vec;
 use std::collections::HashMap;
 use std::uint;
 use std::rc::Rc;
 
+#[deriving(Show)]
 struct Point {
     x: f64,
     y: f64,
 }
 
+#[deriving(Show)]
 struct ClusteredPoint {
     point: Point,
     cluster: i32,
@@ -49,11 +54,43 @@ fn solve(points: &vec::Vec<Rc<ClusteredPoint>>, point: &Point, dist: |&Point, &P
 }
 
 
-fn main() {
-    let mut points = vec::Vec::new();
-    for i in range(0i, 10i) {
-        let p = Rc::new(ClusteredPoint {point: Point {x: i as f64, y: 0.0f64}, cluster: i as i32});
-        points.push(p);
+fn leave_one_out(points: &vec::Vec<Rc<ClusteredPoint>>, k: uint) -> uint {
+    let mut mut_points = points.clone();
+    let mut success_count = 0u;
+    for i in range(0u, mut_points.len()) {
+        let p = mut_points.remove(i).unwrap();
+        if solve(&mut_points, &p.point, distance, k) == p.cluster {
+            success_count += 1;
+        }
+        mut_points.insert(i, p);
     }
-    println!("{}", solve(&points, &Point{x: 10.0f64, y: 0.0f64}, distance, 1));
+    return success_count
+}
+
+
+fn main() {
+    let args = os::args();
+    assert_eq!(args.len(), 2);
+    let path = Path::new(args[1].clone());
+    let mut file = BufferedReader::new(File::open(&path));
+
+    let mut points = vec::Vec::new();
+    for line in file.lines() {
+        let line_str = line.unwrap();
+        let elems = line_str.split_str(",").collect::<Vec<&str>>();
+        assert_eq!(elems.len(), 3);
+
+        let x = from_str::<f64>(elems[0]).unwrap();
+        let y = from_str::<f64>(elems[1]).unwrap();
+        let cluster = from_str::<i32>(elems[2].trim()).unwrap();
+        points.push(Rc::new(ClusteredPoint {point: Point {x: x, y: y}, cluster: cluster}));
+    }
+
+    println!("{} points", points.len());
+
+    for k in range(1u, points.len()) {
+        let success_count = leave_one_out(&points, k);
+        println!("k = {}: {}", k, success_count);
+    }
+
 }
