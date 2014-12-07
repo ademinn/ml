8dc029528112b461dab7ddf4a2736c21b712ac1b
Andrey Demin <ademinn@gmail.com>
2014-12-07 16:58:30 +0300

    gradient descent

--- src/linear/main.rs
+++ src/linear/main.rs
@@ -5,20 +5,39 @@ use std::io::BufferedReader;
 use std::io::File;
 use std::num::Float;
 use std::vec;
-use std::collections::HashMap;
-use std::uint;
-use std::rc::Rc;
 use rgsl::types::vector::VectorF64;
 use rgsl::types::matrix::MatrixF64;
-use rgsl::linear_algebra::SV_decomp;
+use rgsl::blas::level1;
 use rgsl::blas::level2;
-use rgsl::blas::level3;
 use rgsl::cblas::Transpose;
 
 fn build_vector(data: &vec::Vec<f64>) -> VectorF64 {
-    let mut result = VectorF64::from_slice(data.as_slice()).unwrap();
+    let result = VectorF64::from_slice(data.as_slice()).unwrap();
     result.add_constant(-result.min());
     result.scale(1.0f64 / result.max());
+    return result
+}
+
+fn gradient_descent(x0: &VectorF64, lambda: f64, func: |&VectorF64| -> f64, grad: |&VectorF64| -> VectorF64, eps: f64) -> VectorF64 {
+    let mut x1 = x0.clone().unwrap();
+    loop {
+        let x2 = grad(&x1);
+        x2.scale(-lambda);
+        let xdiff = level1::dnrm2(&x2);
+        x2.add(&x1);
+        if (xdiff < eps) || ((func(&x2) - func(&x1)).abs() < eps) {
+            return x2
+        } else {
+            x1 = x2;
+        }
+    }
+}
+
+fn sum_vec(v: &VectorF64) -> f64 {
+    let mut result = 0.0f64;
+    for i in range(0, v.len()) {
+        result += v.get(i);
+    }
     return result
 }
 
@@ -29,6 +48,8 @@ fn main() {
     let mut file = BufferedReader::new(File::open(&path));
 
     let dim = 3;
+    let lambda = 0.01f64;
+    let eps = 0.00001f64;
 
     let mut x1_vec = vec::Vec::new();
     let mut x2_vec = vec::Vec::new();
@@ -48,34 +69,38 @@ fn main() {
 
     let n = x1_vec.len() as u64;
 
-    let mut x0 = VectorF64::new(n).unwrap();
+    let x0 = VectorF64::new(n).unwrap();
     x0.set_all(1.0f64);
     let x1 = build_vector(&x1_vec);
     let x2 = build_vector(&x2_vec);
     let y = VectorF64::from_slice(y_vec.as_slice()).unwrap();
-    let mut f = MatrixF64::new(n, dim).unwrap();
+    let f = MatrixF64::new(n, dim).unwrap();
     f.set_col(0, &x0);
     f.set_col(1, &x1);
     f.set_col(2, &x2);
     let f_orig = f.clone().unwrap();
-    let mut u = MatrixF64::new(dim, dim).unwrap();
-    let mut d_vec = VectorF64::new(dim).unwrap();
-    let mut work = VectorF64::new(dim).unwrap();
-    let res = SV_decomp(&f, &u, &d_vec, &work);
-    let mut d = MatrixF64::new(dim, dim).unwrap();
-    for i in range(0, dim) {
-        d.set(i, i, 1.0f64 / d_vec.get(i));
-    }
-    let mut ud = MatrixF64::new(dim, dim).unwrap();
-    level3::dgemm(Transpose::NoTrans, Transpose::NoTrans, 1.0f64, &u, &d, 1.0f64, &mut ud);
-    let mut fplus = MatrixF64::new(dim, n).unwrap();
-    level3::dgemm(Transpose::NoTrans, Transpose::Trans, 1.0f64, &ud, &f, 1.0f64, &mut fplus);
-    let mut alpha = VectorF64::new(dim).unwrap();
-    level2::dgemv(Transpose::NoTrans, 1.0f64, &fplus, &y, 1.0f64, &mut alpha);
+    let func = |v: &VectorF64| -> f64 {
+        let mut res = y.clone().unwrap();
+        level2::dgemv(Transpose::NoTrans, 1.0f64, &f, v, -1.0f64, &mut res);
+        return level1::dnrm2(&res).powi(2i32)
+    };
+    let grad = |v: &VectorF64| -> VectorF64 {
+        let mut tmp = y.clone().unwrap();
+        let mut res = VectorF64::new(dim).unwrap();
+        level2::dgemv(Transpose::NoTrans, 1.0f64, &f, v, -1.0f64, &mut tmp);
+        for i in range(0, dim) {
+            let (f_col, _) = f.get_col(i).unwrap();
+            f_col.mul(&tmp);
+            res.set(i, sum_vec(&f_col));
+        }
+        return res
+    };
+
+    let alpha = gradient_descent(&VectorF64::new(dim).unwrap(), lambda, func, grad, eps);
+    println!("alpha = {}\n", alpha);
     let mut y_predict = VectorF64::new(n).unwrap();
     level2::dgemv(Transpose::NoTrans, 1.0f64, &f_orig, &alpha, 1.0f64, &mut y_predict);
-    println!("y_predict = \n{}", y_predict);
-    let mut y_diff = y.clone().unwrap();
+    let y_diff = y.clone().unwrap();
     y_diff.sub(&y_predict);
     for i in range(0, n) {
         println!("{} {} -> {} {}%", y.get(i), y_predict.get(i), y_diff.get(i), 100.0f64 * y_diff.get(i) / y.get(i));
